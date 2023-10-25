e8a1ded7076c6df6f056cec15d5a3ef3b38ebee1
Tech-goodat <felixkiprotich2000@gmail.com>
2023-10-25 15:19:03 +0300

    done

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,11 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return <div id="home">
+    <h1 style={{color:"firebrick"}}>Your Name is a Web Developer from Your City</h1>
+    <name />
+    <city />
+  </div>;
 }
 
 export default Home;
